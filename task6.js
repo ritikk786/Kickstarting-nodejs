092ca30c554111c537e6367795278f3335a65ae3
Ritik Kumar <kumarritik9540@gmail.com>
2023-08-02 22:30:51 +0530

    routes match

--- task6.js
+++ task6.js
@@ -2,19 +2,29 @@ const http = require('http')
 
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
+
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log('this is middleware')
-    next()
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add',(req,res,next)=>{
+    console.log('this is app middleware')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="number" name="range"> <button type="submit" >Submit</button></form>')
+    
 })
-app.use((req,res,next)=>{
-    console.log('this is another middleware')
-    next()
+
+app.use('/product',(req,res,next)=>{
+    // res.send(`<h1>${req.body}</h1>`)
+    console.log(req.body)
+    // res.redirect('/')
 })
-app.use((req,res,next)=>{
+
+app.use('/',(req,res,next)=>{
     console.log('this is third middleware')
     res.send('<h1>hello this is 200 code because I made some changes</h1>')
 })
+
 
 app.listen(3000)
